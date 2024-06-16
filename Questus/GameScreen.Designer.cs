4dc60a7da49fcddd753871b4bc5685c9dd767f0e
Cloufers <69718347+Cloufers@users.noreply.github.com>
2024-06-16 19:27:19 +0300

    fix: fixed moving backround

--- Questus/GameScreen.Designer.cs
+++ Questus/GameScreen.Designer.cs
@@ -31,19 +31,12 @@ namespace Questus
         /// </summary>
         private void InitializeComponent()
         {
-            splitter1 = new Splitter();
             Option1 = new Button();
             Option2 = new Button();
             Option3 = new Button();
             SuspendLayout();
             // 
-            // splitter1
             // 
-            splitter1.Location = new Point(0, 0);
-            splitter1.Name = "splitter1";
-            splitter1.Size = new Size(3, 681);
-            splitter1.TabIndex = 1;
-            splitter1.TabStop = false;
             // 
             // Option1
             // 
@@ -53,7 +46,6 @@ namespace Questus
             Option1.Name = "Option1";
             Option1.Size = new Size(538, 54);
             Option1.TabIndex = 0;
-            Option1.Text = "HELP";
             Option1.UseVisualStyleBackColor = true;
             // 
             // Option2
@@ -64,7 +56,6 @@ namespace Questus
             Option2.Name = "Option2";
             Option2.Size = new Size(538, 54);
             Option2.TabIndex = 1;
-            Option2.Text = "button2";
             Option2.UseVisualStyleBackColor = true;
             // 
             // Option3
@@ -75,7 +66,6 @@ namespace Questus
             Option3.Name = "Option3";
             Option3.Size = new Size(539, 54);
             Option3.TabIndex = 2;
-            Option3.Text = "button3";
             Option3.UseVisualStyleBackColor = true;
             // 
             // GameScreen
@@ -83,11 +73,9 @@ namespace Questus
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.scene0;
-            ClientSize = new Size(1264, 681);
             Controls.Add(Option3);
             Controls.Add(Option2);
             Controls.Add(Option1);
-            Controls.Add(splitter1);
             ForeColor = Color.Orange;
             Name = "GameScreen";
             Text = "Game";
@@ -95,7 +83,6 @@ namespace Questus
         }
 
         #endregion
-        private Splitter splitter1;
         private Button Option1;
         private Button Option2;
         private Button Option3;
